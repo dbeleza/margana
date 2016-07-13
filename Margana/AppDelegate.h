4784c908e157ed92ebfe7a2574dc31bbecf1ddee
David Beleza <david@medicineone.net>
2016-07-13 11:08:19 +0100

    Start working on project

---
+++ Margana/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Margana
+//
+//  Created by David Beleza on 13/07/16.
+//  Copyright © 2016 David Beleza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Margana/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Margana
+//
+//  Created by David Beleza on 13/07/16.
+//  Copyright © 2016 David Beleza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
